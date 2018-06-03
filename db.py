6fe5885d6b1880a1547a1b44dc09cfc52517a679
Rohan Barman <rohan1barman@gmail.com>
2018-06-02 22:22:44 -0500

    select -> get obs where price is null

--- db.py
+++ db.py
@@ -4,8 +4,11 @@ import psycopg2
 ################
 # Logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 ################
+
+#TODO: need a dictionary of all tables + columns?
+
 
 def save_articles(articles):
 
@@ -40,6 +43,42 @@ def save_articles(articles):
 			else:
 				# Potentially misleading because we may have upserted
 				logger.info("Successfully entered into db")
+
+	finally:
+		if conn is not None:
+			conn.close()
+
+
+def set_market_price():
+	logger.debug("in set_market_price")
+
+	conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(p.get('host'),p.get('dbname'),p.get('user'),p.get('password'))
+	logger.debug(conn_string)
+	conn = None
+
+	try:
+		conn = psycopg2.connect(conn_string)		
+
+	except (Exception, psycopg2.DatabaseError) as error:
+			logger.debug(error)
+	else:
+		logger.debug("Successfully connected to db")
+		# 1. Get url and timestamps 
+		sql = """ 
+			select url, article_read_date 
+			from article
+			where market_price is null;
+		"""
+		cur = conn.cursor()
+		cur.execute(sql)
+		rows = cur.fetchall()
+		
+		# DEBUG:db:rows is a <class 'list'>
+		# DEBUG:db:a row is a <class 'tuple'>
+		# DEBUG:db:url is a <class 'str'>
+		# DEBUG:db:timestamp is a <class 'datetime.datetime'>
+
+		# Now need to get price closest to time
 
 	finally:
 		if conn is not None:
