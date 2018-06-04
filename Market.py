ca62e196a38af51f1def259684db742f4b1324c7
Rohan Barman <rohan1barman@gmail.com>
2018-06-04 10:35:39 -0500

    get market price for each article * db.set_market_price() and market.get_minute_close_batch() * Select articles that don't have a current market_price and update the market_price * NOTE: db.set_market_price() is seperate from db.save_articles()

--- Market.py
+++ Market.py
@@ -7,7 +7,7 @@ import logging
 ################
 # Logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 ################
 
 # DJI, SPX
@@ -19,3 +19,65 @@ def get_latest_close(symbol):
 	close = close.values[0] # get the actual value
 	logger.info("current price is {}".format(close))
 	return close
+
+# Get minute close for rows
+	# rows have url and time(DateTime)
+	# used by db.set_market_price()
+def get_minute_close_batch(symbol,rows):
+	logger.debug("get_minute_close_batch")
+
+	ts = TimeSeries(key=alphavantage.get('API_KEY'),output_format='pandas')
+	data, meta_data = ts.get_intraday(symbol=symbol,interval='1min')
+
+	close_prices = []
+
+	for row in rows:
+		time = row[1]
+		new_dt = time.replace(second = 0)
+		new_str_time = new_dt.strftime('%Y-%m-%d %H:%M:%S')
+
+		obs = None
+		try:
+			obs = data.loc[new_str_time]
+		# KeyError when data does not contain this date
+		except (Exception, KeyError) as error:
+			logger.debug(error)
+			# TODO: Better value to add? 
+				# Eventually would have to remove this row from db table if the article time is outside of market hours?
+			close_prices.append(-1)
+		else:
+			close = obs['4. close']
+			#logging.info("The close at {} is {}".format(new_str_time,close))
+			close_prices.append(close)
+
+	logger.debug("Returning close_prices")
+	return close_prices
+
+#TODO: accept an array of times to avoid
+	# ValueError: Please consider optimizing your API call frequency.
+def get_minute_close(symbol,time):
+	logger.debug(time)
+	ts = TimeSeries(key=alphavantage.get('API_KEY'),output_format='pandas')
+	data, meta_data = ts.get_intraday(symbol=symbol,interval='1min')
+
+	# the returned data will be a dataframe 
+		# index is a time string with format: '2018-06-01 14:22:00'
+		# columns are open, close, etc
+
+	# convert the datetime object to a string that matches the index format
+		# example: 
+	new_dt = time.replace(second = 0)
+	new_str_time = new_dt.strftime('%Y-%m-%d %H:%M:%S')
+
+	# use this string to search the data by index
+	obs = None
+	try:
+		obs = data.loc[new_str_time]
+	# KeyError when data does not contain this date
+	except (Exception, KeyError) as error:
+		logger.debug(error)
+		return None 
+	else:
+		close = obs['4. close']
+		logging.info("The close at {} is {}".format(new_str_time,close))
+		return close
--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 from config import postgres_config as p
+from Market import get_minute_close
+from Market import get_minute_close_batch
 import logging
 import psycopg2
 ################
 # Logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 ################
+
+#TODO: need a dictionary of all tables + columns?
+
 
 def save_articles(articles):
 
@@ -25,6 +30,7 @@ def save_articles(articles):
 		# save all articles w/ current market price
 		for article in articles:
 			try:
+				# TODO: do I need to reconnect here?
 				conn = psycopg2.connect(conn_string)		
 
 				# upserting to avoid adding duplicate articles
@@ -40,6 +46,55 @@ def save_articles(articles):
 			else:
 				# Potentially misleading because we may have upserted
 				logger.info("Successfully entered into db")
+
+	finally:
+		if conn is not None:
+			conn.close()
+
+def set_market_price():
+	logger.debug("in set_market_price")
+
+	conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(p.get('host'),p.get('dbname'),p.get('user'),p.get('password'))
+	logger.debug(conn_string)
+	conn = None
+
+	try:
+		conn = psycopg2.connect(conn_string)		
+
+	except (Exception, psycopg2.DatabaseError) as error:
+			logger.debug(error)
+	else:
+		logger.debug("Successfully connected to db")
+		# 1. Get url and timestamps 
+		sql = """ 
+			select url, article_read_date 
+			from article
+			where market_price is null;
+		"""
+		cur = conn.cursor()
+		cur.execute(sql)
+		rows = cur.fetchall()
+		
+		close_prices = get_minute_close_batch('DJI',rows)
+		# need to update the market price market_price column
+
+		# for each row, update the market_price column 
+			# rows and close_prices should always be same length
+		for row, close in zip(rows, close_prices):
+			try:
+				sql = """ 
+					UPDATE article
+                	SET market_price = %s
+                	WHERE url = %s
+                """
+				cur = conn.cursor()
+				cur.execute(sql, (close,row[0],))
+				conn.commit()
+				cur.close()
+			except (Exception, psycopg2.DatabaseError) as error:
+				logger.debug(error)
+			else:
+				logger.info("Successfully updated market_price")
 
 	finally:
 		if conn is not None:
